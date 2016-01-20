b490306422889ec57ca3894572bd137131a07dfb
Cody Pete <codypeterson@Claytons-MacBook-Air.local>
2016-01-20 16:07:05 -0600

    Revert "Revert "Main and Conf""
    This reverts commit 1667e0d12f097457c40b2f89df471cafdd296889.

---
+++ main.lua
@@ -0,0 +1,19 @@
+function love.load(args)
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  
+end
+
+function love.mousepressed(x, y, button)
+  
+end
+
+function love.mousereleased(x, y, button)
+  
+end
