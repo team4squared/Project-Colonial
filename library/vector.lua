ca465986eda1946585313bed7df6513c3a4304a7
Cody Pete <codypeterson@Claytons-MacBook-Air.local>
2016-01-31 20:02:57 -0600

    Fixed btw function

--- library/vector.lua
+++ library/vector.lua
@@ -37,8 +37,8 @@ function Vec2(x, y)
     return self.x, self.y
   end
   
-  self.between = function(vec, a, b)
-    return math.btw(vec.x, a.x, b.x) and math.btw(vec.y, a.y, b.y)
+  self.between = function(a, b)
+    return math.btw(self.x, a.x, b.x) and math.btw(self.y, a.y, b.y)
   end
   
   return setmetatable(self, {
