c9cd5fe3d8a01b8771bf4c67c956862dd9e64047
Cody Pete <codypeterson@Claytons-MacBook-Air.local>
2016-01-31 20:37:36 -0600

    Final touches to printer

--- library/printer.lua
+++ library/printer.lua
@@ -1,21 +1,32 @@
 
 printer = {}
 
-function printer:new(x, y)
+function printer:new(x, y, lf)
   local p = {}
   p.x = x
   p.y = y
   p.lineSpace = 15
-  p.maxLines = 10
+  p.maxLines = 5
   p.lines = 0
+  p.lineLife = lf or 5
   p.lineList = {}
   
   function p:write(m)
     if self.lines < self.maxLines then
-      table.insert(self.lineList, m)
+      table.insert(self.lineList, {msg = m, life = 0})
+      self.lines = self.lines + 1
     else
       table.remove(self.lineList, 1)
-      table.insert(self.lineList, m)
+      table.insert(self.lineList, {msg = m, life = 0})
+    end
+  end
+  
+  function p:update(dt)
+    for i, m in pairs(self.lineList) do
+      m.life = m.life + dt
+      if m.life >= self.lineLife then
+        table.remove(self.lineList, i)
+      end
     end
   end
   
@@ -26,7 +37,7 @@ function printer:new(x, y)
       else
         love.graphics.setColor(0, 255, 0)
       end
-      love.graphics.print(m, self.x, self.y)
+      love.graphics.print(m.msg, self.x, self.y + (i - 1) * self.lineSpace)
     end
   end
   
