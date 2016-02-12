340194c6468e5c0baee736694d8838ebf343c46b
Cody Pete <codypeterson@Claytons-MacBook-Air.local>
2016-02-12 16:24:45 -0600

    Heading back to step 1
    Removed the player and portal files. I don’t want everyone else to be
    able to contribute to what we do

--- main.lua
+++ main.lua
@@ -5,27 +5,20 @@ function love.load(args)
   require("library.vector")
   require("entity")
   require("input")
-  require("player")
-  require("portal")
-  
-  printr = printer:new(5, 5)
-  printr:write("Init Printer")
   
   input:init()
-  player:init(love.graphics.getWidth()/2, love.graphics.getHeight()/2)
-  portal:debugGen()
+  printr = printer:new(5, 5)
+  printr:write("Init Printer")
 end
 
 function love.update(dt)
   mouse.x, mouse.y = love.mouse.getPosition()
   entity:update(dt)
-  printr:update(dt)
 end
 
 function love.draw()
   entity:draw()
   input:draw()
-  printr:draw()
 end
 
 function love.mousepressed(x, y, button)
